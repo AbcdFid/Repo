5371b1174b6b63bf48b4a600ff652cb4889dbd71
AbcdFid <filwinford@gmail.com>
2026-04-19 11:26:30 +0700

    removed features

--- my_app.py
+++ my_app.py
@@ -6,12 +6,6 @@ from PyQt5.QtWidgets import(
 from instr import*
 from second_win import*
 
-txt_hello = 'Selamat datang di program deteksi status kesehatan'
-txt_title = 'health'
-txt_next = 'start'
-txt_instruction = 'This application allows you to use the Rufier test to make an initial diagnosis of your health. The Rufier test is a set of physical exercises designed to assess your cardiac performance during physical exertion. The subject lies in the supine position for 5 minutes and has their pulse rate measured for 15 seconds; then, within 45 seconds, the subject performs 30 squats. When the exercise ends, the subject lies down and their pulse is measured again for the first 15 seconds and then for the last 15 seconds of the first minute of the recovery period. Important! If you feel unwell during the test (dizziness, tinnitus, shortness of breath, etc.), stop the test and consult a physician.'
-win_width, win_height= 1000, 600
-win_x, win_y= 200, 100
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
