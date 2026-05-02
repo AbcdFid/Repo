1cf6c9a455c5c95396750fe5d4e77b9bd51fc001
AbcdFid <filwinford@gmail.com>
2026-05-02 22:13:11 +0700

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -12,22 +12,10 @@ from instr import *
 
 class FinalWin(QWidget):
    def __init__(self, exp):
-       ''' the window in which the survey is being conducted '''
-       super().__init__()
-
-
-       #getting data about the experiment
-       self.exp = exp
-
-
-       # creating and configuring graphic elements:
+       super().__init__()  
+       self.exp = exp 
        self.initUI()
-
-
-       # sets what the window will look like (label, size, location)
        self.set_appear()
-      
-       # start:
        self.show()
 
 
@@ -94,18 +82,16 @@ class FinalWin(QWidget):
 
 
    def initUI(self):
-       ''' creates graphic elements '''
+       
        self.work_text = QLabel(txt_workheart + self.results())
        self.index_text = QLabel(txt_index + str(self.index))
-
-
        self.layout_line = QVBoxLayout()
        self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
        self.layout_line.addWidget(self.work_text, alignment = Qt.AlignCenter)        
        self.setLayout(self.layout_line)
 
 
-   ''' sets what the window will look like (label, size, location) '''
+ 
    def set_appear(self):
        self.setWindowTitle(txt_finalwin)
        self.resize(win_width, win_height)
